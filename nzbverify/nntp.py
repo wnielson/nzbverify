ace1f93787056e500826ba8a8f02cbc802f2b22d
Weston <wnielson@gmail.com>
2013-03-18 13:16:01 -0300

    Changed logging level.

--- nzbverify/nntp.py
+++ nzbverify/nntp.py
@@ -152,6 +152,6 @@ class NNTP(nntplib.NNTP):
             use_ssl = True
 
         if _have_ssl and use_ssl:
-            log.info("Using SSL")
+            log.debug("Using SSL")
             return ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1)
         return sock
